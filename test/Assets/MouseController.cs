98d33508c00233ff1d5e36d73a8099790a5f3e10
JoaoTeixeira <joaocostateixeira@ua.pt>
2020-05-11 17:23:52 +0100

    first

---
+++ test/Assets/MouseController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+
+    public float mouseSens = 200f;
+    public Transform playerBody;
+    public Transform leftArm;
+    float xRotation = 0f;
+    float yRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
+        float mousey = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
+
+        xRotation -= mousey;
+        yRotation -= mouseX;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+        Debug.Log(xRotation);
+        if(xRotation != 90f && xRotation != -90f)
+        {
+            leftArm.Rotate(Vector3.left * mousey);
+        }
+       
+
+    }
+}
---
+++ test/Assets/PlayerMovement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float grafity = -11f;
+    public float speed = 12f;
+    Vector3 velocity;
+
+    public Transform gorundCheck;
+    public float jumpHeight = 3f;
+    public float groundDistance = 0.4f;
+
+    public LayerMask groundMaks;
+    bool sprint = false;
+
+    bool isGrounded;
+    // Update is called once per frame
+    void Update()
+    {
+
+        isGrounded = Physics.CheckSphere(gorundCheck.position, groundDistance, groundMaks);
+
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            sprint = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+
+            sprint = false;
+        }
+
+        if (isGrounded)
+        {
+            if (sprint)
+            {
+                speed = 24f;
+            }
+            else
+            {
+                speed = 12f;
+            }
+        }
+    
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * grafity);
+        }
+
+        velocity.y += grafity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
